db1eefe43ccfe0eec629e68bc43c3a45d7885e72
EFlaminggo <electricflaminggo@gmail.com>
2023-10-24 15:16:15 +0700

    Tambahan List Restaurant

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,4 +2,3 @@ import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 
 console.log('Hello Coders! :)');
-
---
+++ src/scripts/script.js
@@ -0,0 +1,30 @@
+// Mendapatkan referensi ke elemen div yang akan menampilkan daftar restoran
+const restaurantList = document.getElementById("restaurant-list");
+
+// Mengambil data dari file JSON
+fetch("/src/public/data/DATA.json")
+  .then((response) => response.json())
+  .then((data) => {
+    const restaurants = data.restaurants;
+
+    // Loop melalui data restoran dan tambahkan mereka ke daftar restoran
+    restaurants.forEach((restaurant) => {
+      const restaurantDiv = document.createElement("div");
+      restaurantDiv.className = "restaurant";
+
+      // Menambahkan informasi restoran ke elemen div
+      restaurantDiv.innerHTML = `
+        <h2>${restaurant.name}</h2>
+        <img src="${restaurant.pictureId}" alt="${restaurant.name} Image">
+        <p>Kota: ${restaurant.city}</p>
+        <p>Rating: ${restaurant.rating}</p>
+        <p>Deskripsi: ${restaurant.description}</p>
+      `;
+
+      // Menambahkan elemen div restoran ke daftar restoran
+      restaurantList.appendChild(restaurantDiv);
+    });
+  })
+  .catch((error) => {
+    console.error("Error fetching data:", error);
+  });
